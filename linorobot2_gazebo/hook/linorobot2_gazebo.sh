ab50e400b5a6857268d700c336e7a8cb2db32eb1
Paul Bouchier <paul.bouchier@gmail.com>
2026-03-13 17:15:04 +0300

    chessboard with king and robot

--- linorobot2_gazebo/hook/linorobot2_gazebo.sh
+++ linorobot2_gazebo/hook/linorobot2_gazebo.sh
@@ -1,1 +1,4 @@
-# prepend-non-duplicate;GZ_SIM_RESOURCE_PATH;share;@CMAKE_INSTALL_PREFIX@/share
+# Prepend the models directory to GZ_SIM_RESOURCE_PATH
+# This allows Gazebo Sim (Harmonic) to find models in the linorobot2_gazebo/models directory
+export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$AMENT_CURRENT_PREFIX/share/linorobot2_gazebo/models
+export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:$AMENT_CURRENT_PREFIX/share/linorobot2_gazebo/models
